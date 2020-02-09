64a682364645b9ba60122c73be9cdc8c72c2fe13
LondonComputadores <london.computadores@gmail.com>
2020-02-09 15:48:17 -0400

    criado atributo complexo filhos

--- orientacao_objetos/classe_e_composicao/oop/pessoa.py
+++ orientacao_objetos/classe_e_composicao/oop/pessoa.py
@@ -1,17 +1,21 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=36):
+    def __init__(self, *filhos, nome=None, idade=36):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá! {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Ahlex')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Alexandre'
-    print(p.nome)
-    print(p.idade)
+    alex = Pessoa(nome='Ahlex')
+    london = Pessoa(alex, nome='London')
+    print(Pessoa.cumprimentar(london))
+    print(id(alex))
+    print(alex.cumprimentar())
+    print(alex.nome)
+    alex.nome = 'Alexandre'
+    print(alex.nome)
+    print(alex.idade)
+    for filho in london.filhos:
+        print(filho.nome)
