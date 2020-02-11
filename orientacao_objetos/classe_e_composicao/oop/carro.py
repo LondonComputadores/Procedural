f46680fe882c09c37b107840dacaaf21b8455e6a
LondonComputadores <london.computadores@gmail.com>
2020-02-10 22:56:25 -0400

    classe Motor implementada

--- orientacao_objetos/classe_e_composicao/oop/carro.py
+++ orientacao_objetos/classe_e_composicao/oop/carro.py
@@ -101,3 +101,10 @@ os seguintes atributos:
 class Motor:
     def __init__(self):
         self.velocidade = 0
+
+    def acelerar(self):
+        self.velocidade += 1
+
+    def frear(self):
+        self.velocidade -= 2
+        self.velocidade = max(0, self.velocidade)
